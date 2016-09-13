d3b686ca4a15ceb332ca5248def091f0ab5e56c7
dhillonsh <dhillonsh@gmail.com>
2016-09-13 14:47:16 -0700

    Update pageserver.py

--- pageserver.py
+++ pageserver.py
@@ -86,7 +86,7 @@ def respond(sock):
         transmit(STATUS_OK, sock)
         size = os.path.getsize('./pages/' + page)
         transmit('content-length: ' + str(size) + "\n", sock)
-        transmit(:status: 200\n", sock)
+        transmit("status: 200\n", sock)
         transmit("vary: Accept-Encoding\n", sock)
 
         if page.endswith(".html"):
